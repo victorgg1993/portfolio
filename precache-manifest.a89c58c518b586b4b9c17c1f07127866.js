b66dd27205b8f57035a986c8913a0e1ede892014
victorgg1993 <vigomga1993@gmail.com>
2021-02-14 22:02:48 +0100

    Updates

---
+++ precache-manifest.a89c58c518b586b4b9c17c1f07127866.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2251873a258e59f1c9478a3553059c61",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a50f071f1af1fc3726a3",
+    "url": "/portfolio/static/css/2.8cf600a9.chunk.css"
+  },
+  {
+    "revision": "6eca7db98b99c910c974",
+    "url": "/portfolio/static/css/main.2b1f2f87.chunk.css"
+  },
+  {
+    "revision": "a50f071f1af1fc3726a3",
+    "url": "/portfolio/static/js/2.e72da58e.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/portfolio/static/js/2.e72da58e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eca7db98b99c910c974",
+    "url": "/portfolio/static/js/main.ff615840.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  }
+]);
